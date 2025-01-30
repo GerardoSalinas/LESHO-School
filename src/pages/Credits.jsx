2112e2fa7d4785be4864096707d5c88f974f87af
gasalinas <gerardoaplicano16@gmail.com>
2025-01-30 17:57:51 -0600

    Defines credits page layout

--- src/pages/Credits.jsx
+++ src/pages/Credits.jsx
@@ -1,7 +1,26 @@
+import { Box, Typography, Divider, Container } from "@mui/joy"
+import { CarousselSlider } from "../components/CarousselSlider/CarousselSlider";
+import { mainGroup } from "./../components/CarousselSlider/groupMembersInfo"
+
 export function Credits(){
-    return (
-        <>
-            <p>This is Credits!</p>
-        </>
-    )
+  return (
+    <Container
+      sx={{
+        display: "flex",
+        width: "100vw",
+        height: "70vh",
+        alignItems: "flex-start",
+      }}
+    >
+      <Box sx={{ width: "100%", marginX: { sm: '1vw', lg: '5vw' }  }}>
+        <Typography variant="h6" component="h6" sx={{ color:'#1E1E1E' }}>
+          Equipo Principal
+        </Typography>
+        <Divider />
+
+        <CarousselSlider  items={mainGroup} />
+
+      </Box>
+    </Container>
+  );
 }
