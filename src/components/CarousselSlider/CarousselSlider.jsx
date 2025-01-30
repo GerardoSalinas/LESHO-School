6b7a78a61ce19a52c669aa54123782d76688769c
gasalinas <gerardoaplicano16@gmail.com>
2025-01-30 17:52:56 -0600

    Creates CarousselSlider

---
+++ src/components/CarousselSlider/CarousselSlider.jsx
@@ -0,0 +1,52 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import { TestimonialCard } from './TestimonialCard';
+import { useState } from 'react';
+import { useWindowSize } from "@uidotdev/usehooks";
+import "./styles.css"
+
+export function CarousselSlider({ items }){
+    const seconds = useState(new Date().getSeconds());
+    const milis = useState(new Date().getMilliseconds());
+    const size = useWindowSize();
+    
+    const chunkArray = (arr, size) => {
+        return arr.reduce((acc, _, i) => 
+            (i % size === 0 ? [...acc, arr.slice(i, i + size)] : acc), []);
+    };
+
+    const groupedItems = chunkArray(items,  size.width <= 690 ? 1 : 2 );
+
+    return (
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        navigation={false}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {
+          groupedItems.map((group, index) => (
+            <SwiperSlide key={index}>
+              {group.map((card) => (
+                <TestimonialCard
+                  key={`${card.title}-${seconds}-${milis}`}
+                  imagePath={card.imagePath}
+                  title={card.title}
+                  subtitle={card.subtitle}
+                  body={card.body}
+                />
+              ))}
+            </SwiperSlide>
+          ))
+        }
+      </Swiper>
+    );
+}
