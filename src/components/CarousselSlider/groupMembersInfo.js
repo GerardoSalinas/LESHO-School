62b4fb0fdf7b7bbc5a069b8b47acf07470d94ac8
gasalinas <gerardoaplicano16@gmail.com>
2025-01-30 17:55:54 -0600

    Defines credits info

---
+++ src/components/CarousselSlider/groupMembersInfo.js
@@ -0,0 +1,28 @@
+export const mainGroup = [
+    {
+        imagePath: "/images/Iris_lineth.jpg",
+        title: "Iris Lineth Montes Rivera",
+        subtitle: "Medicina",
+        body: "Usando las distintas capacidades humanas podemos formar un mejor futuro"
+    },
+    {
+        imagePath: "/images/pattern.jpg",
+        title: "Fernando Romero",
+        subtitle: "Ing. Quimica",
+        body: "They make it easy to be consistent with color choices, spacing, typography"
+    },
+    {
+        imagePath: "/images/pattern.jpg",
+        title: "Anthony José Garcia Rodriguez ",
+        subtitle: "Quimica y Farmacia ",
+        body: "Sé el cambio que quieres ver en el mundo"
+    },
+    {
+        imagePath: "/images/pattern.jpg",
+        title: "Carlos Isaac Sánchez Barrientos",
+        subtitle: "Bach. Tec. en Informática",
+        body: "Trata de dejar el mundo en mejores condiciones de las que tenía cuando llegaste a el"
+    },
+];
+
+export const developers = [];
